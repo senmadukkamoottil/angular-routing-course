24b67f99e59e42b35d3e5f4611330f2446a4813d
Sen Madukkamoottil <sentvla@gmail.com>
2025-05-20 11:49:14 -0400

    rouing added in module based code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private readonly router: Router) {
+
+    this.router.events.subscribe(response => {
+      if (response instanceof NavigationStart) {
+        console.log('Navigation started')
+      }
+    })
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  onActivate(event: any) {
+    console.log('Activated route', event);
+  }
 }
